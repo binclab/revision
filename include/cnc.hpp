4d4cf04c6de825981c18fc207ca4025ed07aac46
Bret Joseph <bretjoseph@gmail.com>
2018-12-31 18:45:43 +0200

    modified:   include/cnc.hpp 	modified:   src/cnc.cpp

--- include/cnc.hpp
+++ include/cnc.hpp
@@ -7,18 +7,24 @@
 class CNCWindow : public Gtk::Window
 {
 public:
-  CNCWindow();
-  virtual ~CNCWindow();
+    CNCWindow();
+    virtual ~CNCWindow();
 
 protected:
-  //Signal handlers:
-  void on_button_quit();
+    //Signal handlers:
+    void on_button_quit();
+    void on_title_entry_activate();
+    void on_switch_account();
 
-  //Child widgets:
-  Gtk::Box m_VBox;
-  //WebApp *m_WebApp;
-  Gtk::Notebook games_Notebook;
-  //Gtk::Button m_Button_Quit;
+    //Child widgets:
+    Gtk::Button m_button;
+    Gtk::HeaderBar m_headerbar;
+    Gtk::Toolbar m_toolbar;
+    Gtk::ComboBoxText m_combotext;
+    Gtk::Entry m_title_entry;
+    //WebApp *m_WebApp;
+    Gtk::Notebook m_notebook;
+    //Gtk::Button m_Button_Quit;
 };
 
 #endif //GTKMM_EXAMPLEWINDOW_H
--- src/cnc.cpp
+++ src/cnc.cpp
@@ -2,40 +2,65 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <string>
+#include <map>
 
 CNCWindow::CNCWindow()
-: m_VBox(Gtk::ORIENTATION_VERTICAL)/*,
+/*: m_VBox(Gtk::ORIENTATION_VERTICAL)*//*,
   m_Button_Quit("Quit")*/
 {
-  set_title("Custom Widget example");
-  set_position(Gtk::WIN_POS_CENTER_ALWAYS);
-  set_border_width(6);
-  set_has_resize_grip(true);
-  set_size_request(1024, 576);
-  maximize();
-  resize_to_geometry(1024, 576);
+    maximize();
+    //set_title("Custom Widget example");
+    set_position(Gtk::WIN_POS_CENTER_ALWAYS);
+    set_border_width(6);
+    set_has_resize_grip(true);
+    set_size_request(1024, 576);
+    resize_to_geometry(1024, 576);
+    /*
 
-  WebApp *game_1 = new WebApp;
-  WebApp *game_2 = new WebApp;
-  WebApp *game_3 = new WebApp;
-  WebApp *game_4 = new WebApp;
-  WebApp *game_5 = new WebApp;
+        std::array<std::string, 5> games = {"WebApp_1","WebApp_2","WebApp_3","WebApp_4","WebApp_5"};
+        for(const auto& game : games)
+        {
+            games_Notebook.append_page(*game, "Game 1");
+            game->load_uri("https://tiberiumalliances.com/home");
+        }
+    */
+    on_title_entry_activate();
+    //on_subtitle_entry_activate();
+    // Header Bar
+    {
+        m_headerbar.set_show_close_button();
+        m_headerbar.pack_start(m_toolbar);
+        m_combotext.set_size_request(200, -1);
+        m_combotext.append("Add");
+        //m_combotext.set_active_text("Add");
+        m_headerbar.pack_end(m_combotext);
+        m_combotext.signal_changed().connect( sigc::mem_fun(*this, &CNCWindow::on_switch_account));
 
-  games_Notebook.set_show_tabs(1);
-  add(games_Notebook);
+        set_titlebar(m_headerbar);
+    }
 
-  games_Notebook.append_page(*game_1, "Game 1");
-  game_1->load_uri("https://tiberiumalliances.com/home");
-  games_Notebook.append_page(*game_2, "Game 2");
-  game_2->load_uri("https://tiberiumalliances.com/home");
-  games_Notebook.append_page(*game_3, "Game 3");
-  game_3->load_uri("https://tiberiumalliances.com/home");
-  games_Notebook.append_page(*game_4, "Game 4");
-  game_4->load_uri("https://tiberiumalliances.com/home");
-  games_Notebook.append_page(*game_5, "Game 5");
-  game_5->load_uri("https://tiberiumalliances.com/home");
-  std::cout << "Loading URL" << std::endl;
-  show_all_children();
+    WebApp *game_1 = new WebApp;
+    /*WebApp *game_2 = new WebApp;
+    WebApp *game_3 = new WebApp;
+    WebApp *game_4 = new WebApp;
+    WebApp *game_5 = new WebApp;*/
+
+    m_notebook.set_show_tabs(0);
+    add(m_notebook);
+    m_notebook.append_page(*game_1, "Game 1");
+    game_1->load_uri("https://tiberiumalliances.com/home");
+    /*m_notebook.append_page(*game_2, "Game 2");
+    //game_2->load_uri("https://tiberiumalliances.com/home");
+    m_notebook.append_page(*game_3, "Game 3");
+    //game_3->load_uri("https://tiberiumalliances.com/home");
+    m_notebook.append_page(*game_4, "Game 4");
+    //game_4->load_uri("https://tiberiumalliances.com/home");
+    m_notebook.append_page(*game_5, "Game 5");
+    //game_5->load_uri("https://tiberiumalliances.com/home");
+    m_notebook.get_current_page();*/
+    std::cout << "Loading URL" << std::endl;
+    show_all_children();
 }
 
 CNCWindow::~CNCWindow()
@@ -44,5 +69,33 @@ CNCWindow::~CNCWindow()
 
 void CNCWindow::on_button_quit()
 {
-  hide();
+    hide();
+}
+
+void CNCWindow::on_switch_account()
+{
+    Glib::ustring sz = m_combotext.get_active_text();
+
+    if(sz == "Add")
+    {
+        std::string name = "Account" + std::to_string(m_notebook.get_n_pages() + 1);
+        std::cout << name << std::endl;
+        std::map<std::string, WebApp*> m_accounts;
+        m_combotext.append(m_accounts[name]);
+        m_accounts[name] = new WebApp;
+        m_notebook.append_page(m_accounts[name]);
+    }
+    //if(!(sz.empty()))
+    //    std::cout << sz  << std::endl;
+    // Save session
+    // Swithc to new tab
+}
+
+void CNCWindow::on_title_entry_activate()
+{
+    const Glib::ustring title = m_title_entry.get_text();
+    if(!title.empty())
+    {
+        m_headerbar.set_title(title);
+    }
 }
