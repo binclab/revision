89894ba08a19973c76e642a4be55a4e3da7ba986
Bret Joseph <bretjoseph@gmail.com>
2023-01-30 18:42:00 +0200

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '310224639988557',
+        cookie: true,
+        xfbml: true,
+        version: 'v15.0'
+    });
+
+    FB.AppEvents.logPageView();
+};
+FB.getLoginStatus(function (response) {
+    if (response.status === 'connected') {
+        console.log('Logged In');
+    } else {
+        FB.login();
+    }
+});
